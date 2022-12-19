09e1ca40899fbdb4deb43ca90bd3c51bb64864a9
sebart7 <s3rodriguez10@gmail.com>
2022-12-19 16:35:17 -0500

    Add Service Course

--- gestion-horaria-back/src/main/resources/import.sql
+++ gestion-horaria-back/src/main/resources/import.sql
@@ -10,9 +10,9 @@ INSERT INTO `program` (`program_id`, `name`, `department_id`) VALUES ('PIET', 'I
 INSERT INTO `teacher` (`teacherCode`, `full_name`, `department_id`) VALUES ('1', 'DANIEL PAZ', '1');
 INSERT INTO `teacher` (`teacherCode`, `full_name`, `department_id`) VALUES ('2', 'DELIO HURTADO', '2');
 --Period
-INSERT INTO `period` (`id`, `state`) VALUES ('2021_02', 'FINISHED');
-INSERT INTO `period` (`id`, `state`) VALUES ('2022_01', 'FINISHED');
-INSERT INTO `period` (`id`, `state`) VALUES ('2022_02', 'IN_PROGRESS');
+INSERT INTO `period` (`periodId`, `state`) VALUES ('2021_02', 'FINISHED');
+INSERT INTO `period` (`periodId`, `state`) VALUES ('2022_01', 'FINISHED');
+INSERT INTO `period` (`periodId`, `state`) VALUES ('2022_02', 'IN_PROGRESS');
 --Subject
 INSERT INTO `subject` (`subject_code`, `name`, `semester`, `time_block`, `weekly_overload`, `program_id`) VALUES ('POO', 'Programacion Orientada a Objetos', '1', false, '2', 'PIS');
 INSERT INTO `subject` (`subject_code`, `name`, `semester`, `time_block`, `weekly_overload`, `program_id`) VALUES ('ESTR1', 'Estructura de Datos', '1',false, '3', 'PIS');
