277b739f415e1e661ab1dd2bf3e65f4384deaa7a
paulapc <ppena@unicauca.edu.co>
2023-01-25 00:16:28 -0500

    [front] maqueta profesor y schedule ..

---
+++ gestion-horaria-front/src/app/services/schedule-professor/schedule-professor.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleProfessorService {
+
+  constructor() { }
+
+  // TODO servicios consultar profesores , obtener horario de ese profesor
+}
