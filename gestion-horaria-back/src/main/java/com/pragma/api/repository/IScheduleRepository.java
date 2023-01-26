c005a1cfcaacff643d8308335b51c4b0be5f1abc
JulianSanti28 <juliansmartinez@unicauca.edu.co>
2023-01-26 11:18:24 -0500

    Added: Find all Schedule by Environment

--- gestion-horaria-back/src/main/java/com/pragma/api/repository/IScheduleRepository.java
+++ gestion-horaria-back/src/main/java/com/pragma/api/repository/IScheduleRepository.java
@@ -1,7 +1,11 @@
 package com.pragma.api.repository;
 
+import com.pragma.api.model.Environment;
 import com.pragma.api.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule>findAllByEnvironment(Environment environment);
 }
