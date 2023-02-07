7723f98e7648da47775646579ef3ca9b95c883bf
JulianSanti28 <juliansmartinez@unicauca.edu.co>
2023-02-06 19:39:07 -0500

    Added: Available Courses REST Service

--- gestion-horaria-back/src/main/java/com/pragma/api/security/controllers/AuthenticationController.java
+++ gestion-horaria-back/src/main/java/com/pragma/api/security/controllers/AuthenticationController.java
@@ -6,7 +6,6 @@ import com.pragma.api.security.dto.LoginDTO;
 import com.pragma.api.security.dto.UserDTO;
 import com.pragma.api.security.jwt.JwtProvider;
 import com.pragma.api.security.services.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,7 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 @RestController
